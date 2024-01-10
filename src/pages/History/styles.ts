fab6d81a10faaee997d90fbb2516abd3549f2432
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2024-01-10 17:32:30 -0300

    use reactHookForm

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -79,7 +79,7 @@ export const Status = styled.span<StatusProps>`
     content: '';
     width: 0.5rem;
     height: 0.5rem;
-    border-radius: 9999px;
+    border-radius: 50%;
     background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
