ca3a27d5c2b3b73fc7bd7bdb58488a0bcabf1fc1
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2024-01-10 10:45:07 -0300

    Page History

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home/index.tsx'
-import { History } from './pages/History'
+import { History } from './pages/History/index.tsx'
 import { DefaultLayout } from './layouts/DefaultLayout/index.tsx'
 
 export function Router() {
